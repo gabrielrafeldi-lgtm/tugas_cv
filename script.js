5be10cba6e8e6437c10d25323223b6c3d53fa506
gabriel <gabrielrafeldi@gmail.com>
2026-06-15 12:58:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Menambahkan interaksi sederhana pada tombol kontak
+document.getElementById('btn-kontak').addEventListener('click', function() {
+    alert('Terima kasih sudah berkunjung! Silahkan hubungi saya melalui email atau GitHub yang tertera.');
+});
